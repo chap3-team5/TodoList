940d694f4acce564be408b7d79e72610dc437a26
HaeRheeNam <hanm3208@gmail.com>
2022-10-18 02:39:54 +0900

    📌남해리:1018 am 02:39 할일 추가하기 -form 입력, 추가기능, 추가버튼 구현 완료

--- src/pages/AddTodo.jsx
+++ src/pages/AddTodo.jsx
@@ -1,7 +1,78 @@
 import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { __addBtn } from '../redux/modules/addtodoSlice';
+import Header from '../components/Header';
 
 const AddTodo = () => {
-  return <div>AddTodo</div>;
+  const dispatch = useDispatch();
+
+  // // const { isLoading, error, todos } = useSelector((state) => state);
+
+  const [todo, setTodo] = useState({
+    username: '',
+    title: '',
+    body: '',
+  });
+
+  const onChangeValue = (e) => {
+    // console.log(e);
+    const { name, value } = e.target;
+    console.log(name, value);
+    setTodo({ ...todo, [name]: value });
+  };
+
+  const onAddBtn = () => {
+    if (
+      todo.username.trim() === '' ||
+      todo.title.trim() === '' ||
+      todo.body.trim() === ''
+    ) {
+      return alert('모든 항목을 입력해주세유');
+    }
+    dispatch(__addBtn(todo));
+    setTodo({ username: '', title: '', body: '' });
+  };
+
+  return (
+    <div>
+      <Header />
+      <form onSubmit={onAddBtn}>
+        <div>
+          <label>작성자</label>
+          <input
+            name="username"
+            placeholder="작성자의 이름을 입력해주세요.(5자 이내)"
+            maxLength="5"
+            value={todo.username}
+            onChange={onChangeValue}
+            required
+          />
+          <label>제목</label>
+          <input
+            name="title"
+            placeholder="제목을 입력해주세요.(50자 이내)"
+            maxLength="50"
+            value={todo.title}
+            onChange={onChangeValue}
+            required
+          />
+          <label>내용</label>
+          <textarea
+            name="body"
+            placeholder="내용을 입력해주세요.(200자 이내)"
+            maxLength="200"
+            value={todo.body}
+            onChange={onChangeValue}
+            required
+          />
+          <div className="btnBox">
+            <button>추가하기</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddTodo;
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,7 +1,10 @@
-import { createStore } from 'redux';
-import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import todos from '../modules/todolistSlice';
+import detailTodo from '../modules/detailTodoSlice';
+import addTodos from '../modules/addtodoSlice';
 
-const rootReducer = combineReducers({});
-const store = createStore(rootReducer);
+const store = configureStore({
+  reducer: { detailTodo: detailTodo, todos: todos, addTodos: addTodos },
+});
 
 export default store;
