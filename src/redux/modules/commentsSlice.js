a480ed6db3806831dd10bc6fcdeb34124055e5db
HaeRheeNam <hanm3208@gmail.com>
2022-10-18 20:16:06 +0900

    남해리:pm20:16 comments 수정 및 리듀서 작성, comment 연결

--- src/redux/modules/commentsSlice.js
+++ src/redux/modules/commentsSlice.js
@@ -1,8 +1,38 @@
+//src/redux/modules/commentsSlice.js
+//ActionCreator
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const commentsSlice = () => {
-  return <div>commentsSlice</div>;
-};
+export const __getTodoId = createAsyncThunk(
+  'getTodoId',
+  async (arg, thunkAPI) => {
+    try {
+      const { data } = await axios.get(`localhost:3001/comments?todoId=${arg}`);
+      return thunkAPI.fulfillWithValue(data);
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.code);
+    }
+  }
+);
 
-export default commentsSlice;
+//initialState
+const initialState = {
+  comments: {
+    data: [],
+    isLoading: false,
+    error: null,
+  },
+  commentsTodoId: {
+    data: [],
+    isLoading: false,
+    error: null,
+  },
+};
+//reducer
+export const commentsSlice = createSlice({
+  name: 'comments',
+  initialState,
+  reducer: {},
+});
+//export reducer
+export default commentsSlice.reducer;
