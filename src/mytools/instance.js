82ebaa15506b8fa688ba1279118825fe5ca20be9
goeunrain <boniteterain@gmail.com>
2022-10-20 19:27:40 +0900

    instance

---
+++ src/mytools/instance.js
@@ -0,0 +1,18 @@
+const instance = axios.create({
+  baseURL: process.envproduction.REACT_APP_URL,
+});
+
+export const TodoApi = {
+  getComments: () => instance.get('/comments'),
+  getCommentsId: (payload) => instance.get(`/comments/${payload}`),
+  getCommentsTodoId: (payload) => instance.get(`/comments/?todoId=${payload}`),
+  postComments: (payload) => instance.post('/comments', payload),
+  deleteComments: (payload) => instance.delete(`/comments/${payload}`),
+  patchComments: (payload) => instance.patch(`/comments/${payload.id}`),
+
+  getTodos: () => instance.get('/todos'),
+  getTodos: (payload) => instance.get(`/todos?id=${payload}`),
+  postTodos: (payload) => instance.post('/todos', payload),
+  deleteTodos: (payload) => instance.delete(`/todos/${payload}`),
+  patchTodos: (payload) => instance.patch(`/todos/${payload.id}`),
+};
